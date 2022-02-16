e5a6c0986e28f322ceec9eceeea00206618a255e
lehaontherun <53890564+lehaontherun@users.noreply.github.com>
2022-02-16 21:26:29 +0500

    Update leven.c

--- cdistance/leven.c
+++ cdistance/leven.c
@@ -4,10 +4,10 @@
 #ifndef LEVENSHTEIN_C
 #define LEVENSHTEIN_C
 
-static Py_ssize_t
-minimum(const Py_ssize_t *column, Py_ssize_t len)
+static Py_size_t
+minimum(const Py_size_t *column, Py_size_t len)
 {
-	Py_ssize_t min;
+	Py_size_t min;
 
 	assert(len > 0);
 	min = column[--len];
@@ -18,3 +18,24 @@ minimum(const Py_ssize_t *column, Py_ssize_t len)
 	
 	return min;
 }
+
+#endif
+
+static Py_size_t
+leven(unicode *flow1, unicode *flow2, Py_size_t len1, Py_size_t len2, Py_size_t max_distance)
+{
+	Py_size_t i, j;
+	Py_size_t new, old;
+	Py_size_t cost, dist = -2;
+	Py_size_t *column;
+	
+#ifdef SEQUENCE_COMP
+	int compute;
+#endif
+	
+	if (len1 < len2) {
+		SWAP(unicode *,  flow1, flow2);
+		SWAP(Py_size_t, len1, len2);
+	}
+	
+	
