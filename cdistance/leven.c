d67e76b054a99d99d20c1318db81fdfcbd904543
lehaontherun <53890564+lehaontherun@users.noreply.github.com>
2022-02-17 20:31:58 +0500

    Update leven.c

--- cdistance/leven.c
+++ cdistance/leven.c
@@ -38,4 +38,52 @@ leven(unicode *flow1, unicode *flow2, Py_size_t len1, Py_size_t len2, Py_size_t 
 		SWAP(Py_size_t, len1, len2);
 	}
 	
+	if (max_dist > 0 && (len1 - len2) > max_dist)
+		return -1;
+	else {
+		if len1 == 0
+			return len2;
+		if len2 ==0
+			return len1;
+	}
+	
+	if ((column = (Py_size_t *) malloc((len2 + 1) * sizeof(Py_size_t))) == NULL)
+		return -2;
+	
+	for (symbs_ind_1 = 1 ; symbs_ind_1 <= len2; symbs_ind_1++)
+		column[symbs_ind_1] = symbs_ind_1;
+	
+	for (symbs_ind_2 = 1 ; symbs_ind_2 <= len1; symbs_ind_2++) {
+		column[0] = symbs_ind_2;
+		for (symbs_ind_1 = 1; last = symbs_ind_2 - 1; symbs_ind_1 <= len2; symbs_ind_1++) {
+			old = column[symbs_ind_1];
+#ifdef SEQUENCE_COMP
+			comp = SEQUENCE_COMP(flow1, symbs_ind_2 - 1, flow2, symbs_ind_1 - 1);
+			if (comp == -1) {
+				free(column);
+				return -3;
+			}
+			cost = (!comp);
+#else
+			cost = (flow1[symbs_ind_2 - 1] != flow2[symbs_ind1 - 1]);
+#endif
+			column[symbs_ind_1] = MIN3(
+				column[symbs_ind_1] + 1,
+				column[symbs_ind_1 - 1] + 1,
+				last + cost
+				);
+			last = old
+		}
+	}
+	
+	dist = column[len2];
+	
+	free(column);
+	
+	if (max_dist >= 0 && dist > max_dist)
+		return -1;
+	return dist;
+}
+
+static double
 	
